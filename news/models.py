802de06dbbfd41eb2119a3b5a0e58bdcc2b5c191
anatoliy-zhuk <af3557771@gmail.com>
2025-01-30 00:42:11 +0300

    Lesson 4 - SQL, CRUD выполнен, внесены изменения в db from .json

--- news/models.py
+++ news/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
