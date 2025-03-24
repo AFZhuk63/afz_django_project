da6eb8c335ca3444f704c75bc8d5045cd6a86320
anatoliy-zhuk <af3557771@gmail.com>
2025-03-25 02:33:06 +0300

    Урок 27: класс региУрок 27: форма регистрации пользователя RegisterUserForm

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', views.LogoutUser.as_view(), name='logout'),
-    path('signup/', views.RegisterUser.as_view(), name='signup'),
+    path('signup/', views.sign_up, name='signup'),
     path('register_done/', views.RegisterDoneView.as_view(), name='register_done')
 ]
 
